e35a963114d284208e836f3093b7263bbfb82337
tTanya98 <chichkinat98@gmail.com>
2023-03-11 19:58:34 +0200

    Update tsk-1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,29 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+const galleryEl = document.querySelector(".gallery");
+const galleryIt = createGalleryItemsMarkup(galleryItems);
+
+galleryEl.insertAdjacentHTML("beforeend", galleryIt);
+
+function createGalleryItemsMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<a class="gallery__item" href="${original}">
+    <img class="gallery__image" 
+    src="${preview}" 
+    alt="${description}" />
+  </a>`;
+    })
+    .join("");
+}
+
+const lightbox = new SimpleLightbox(`.gallery__item`, {
+  captionsData: "alt",
+  captionDelay: 250,
+});
 
 console.log(galleryItems);
