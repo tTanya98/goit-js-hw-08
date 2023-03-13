14c23af4a479a33f4ec0f16b44324485be459cf3
tTanya98 <chichkinat98@gmail.com>
2023-03-13 19:45:08 +0200

    Update tsk-3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,30 @@
+import throttle from 'lodash.throttle';
 
+const formEl = document.querySelector('.feedback-form');
+const USER_FORM = 'feedback-form-state';
+
+let formData = JSON.parse(localStorage.getItem(USER_FORM)) || {};
+
+formEl.addEventListener('input', throttle(storageFormData, 500));
+formEl.addEventListener('submit', onFormSubmit);
+reloadPage();
+
+function storageFormData(e) {
+    formData[e.target.name] = e.target.value.trim();
+    localStorage.setitem(USER_FORM, JSON.stringify(formData));
+}
+
+function onFormSubmit(e) {
+    e.preventDefault();
+    if (refs.input.value === "" || refs.textarea.value === "") {
+        return alert(`Please, fill all the fields!`);
+    }
+}
+
+function reloadPage() {
+    if (formData) {
+        let { email, message } = formEl.elements;
+        email.value = formData.email || '';
+        message.value = formData.message || '';
+    }
+}
