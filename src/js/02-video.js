492f5304cd1ca3dbe06b417d4fb912182c559576
tTanya98 <chichkinat98@gmail.com>
2023-03-13 16:52:36 +0200

    Update tsk-2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -2,7 +2,7 @@ import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
 
 const iframe = document.querySelector('iframe');
-const Player = new Player(iframe);
+const player = new Player(iframe);
 
 const STORAGE_TIME_KEY = 'videoplayer-current-time';
 const currentTime = localStorage.getItem(STORAGE_TIME_KEY)
